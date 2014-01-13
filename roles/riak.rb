7ed92b2e3ba8928d08bbd0672a68c2ad55fbe761
Hector Castro <hectcastro@gmail.com>
2014-01-13 18:09:18 -0500

    Update Riak version to 1.4.7.

--- roles/riak.rb
+++ roles/riak.rb
@@ -1,5 +1,30 @@
 name "riak"
 description "Role for Riak nodes."
 run_list(
-  "recipe[riak-ssl]"
+  "recipe[riak]"
+)
+default_attributes(
+  "riak" => {
+    "package" => {
+      "version" => {
+        "major" => 1,
+        "minor" => 4,
+        "incremental" => 7
+      }
+    },
+    "args" => {
+      "+zdbbl" => 32768
+    },
+    "config" => {
+      "riak_core" => {
+        "ring_creation_size" => 8,
+      },
+      "riak_kv" => {
+        "anti_entropy_concurrency" => 1,
+        "map_js_vm_count" => 0,
+        "reduce_js_vm_count" => 0,
+        "hook_js_vm_count" => 0
+      }
+    }
+  }
 )
